e796ea4c188ed7136790236d3381e73f18e05dd9
Designer <154143357@qq.com>
2019-05-23 01:43:25 +0800

    Update girlfriend_text.py

--- girlfriend_text.py
+++ girlfriend_text.py
@@ -3,7 +3,6 @@ import schedule # 记得安装这个库
 
 
 def get_girlfriend():
-    itchat.auto_login(hotReload=True)
     girl_list = itchat.search_friends(name=u'name')  # name替换为对象微信名即可
     girl_name = girl_list[0]['UserName']
     return girl_name
@@ -15,8 +14,9 @@ def send_text(todo):
 
 
 if __name__ == '__main__':
+    itchat.auto_login(hotReload=True)    # 登录
     list = ['小可爱早安','要记得乖乖吃饭哦~~','快去午睡休息啦❤️','乖乖睡觉哦，别熬夜啦，晚安~']
-    schedule.every().day.at('8:30').do(send_text, list[0])    # 早安语
+    schedule.every().day.at('08:30').do(send_text, list[0])    # 早安语
     schedule.every().day.at('12:00').do(send_text, list[1])    # 提醒吃饭
     schedule.every().day.at('13:00').do(send_text, list[2])    # 提醒午睡
     schedule.every().day.at('22:30').do(send_text, list[3])    # 晚安告别
